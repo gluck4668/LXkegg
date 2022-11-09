eb3251c66a9c3d17561db1960ff95baba0a36530
gluck4668 <gluck4668@sina.com>
2022-11-09 23:41:48 +0800

    Initial commit

---
+++ LXkegg_data.R
@@ -0,0 +1,12 @@
+
+setwd("D:/R-lin study/R packages/LXkegg")
+library(openxlsx)
+
+gene_data_example <- read.xlsx("gene_data_example.xlsx")
+
+usethis::use_data(gene_data_example,overwrite = T)
+
+rm(list=ls())
+
+data(gene_data_example)
+
